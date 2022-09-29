a25fd762818d208ba30f97c78da0c479dab1cf3a
Jorge210701 <114266898+Jorge210701@users.noreply.github.com>
2022-09-28 23:42:25 -0500

    Delete ex2.py

--- ex2.py
+++
@@ -1,9 +0,0 @@
-#Ejercicio de arreglos desde 1 hasta n desde consola, teniendo ese arreglo vamos a saber cuáles números son primos
-import sys
-import numpy as np
-def arreglo_1(sys.argv[1]):
-    print("Introduce tu numero")
-    x = int(input())
-    y = np.arange(1,x+1)
-    print(y)
-    return
